e61e3855353ba7b2a4ed2417add391547ca86960
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2022-06-14 09:45:46 +0900

    Nextのバージョンアップと'resolutions'を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
-export default MyApp
+export default MyApp;
